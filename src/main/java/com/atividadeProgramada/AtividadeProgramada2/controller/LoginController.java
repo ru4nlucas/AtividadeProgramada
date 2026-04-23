7ea2aab81e13a11b2080a0a35707dd9723bfc43a
Ruan Lucas <lucaslima1224@gmail.com>
2026-04-22 21:35:26 -0300

    recolocado a dependencia do thymeleaf

--- src/main/java/com/atividadeProgramada/AtividadeProgramada2/controller/LoginController.java
+++ src/main/java/com/atividadeProgramada/AtividadeProgramada2/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.atividadeProgramada.AtividadeProgramada2.entity.Usuario;
@@ -15,6 +16,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 
 @Controller
+@RequestMapping()
 @AllArgsConstructor
 public class LoginController {
 
