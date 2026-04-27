73cf763fadda764dd279ec1b554e1d5ce897fb1d
Ruan Lucas <lucaslima1224@gmail.com>
2026-04-27 20:29:58 -0300

    removido classes nao utilizadas

--- src/main/java/com/atividadeProgramada/AtividadeProgramada2/repository/AlunoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.atividadeProgramada.AtividadeProgramada2.repository;
-
-import com.atividadeProgramada.AtividadeProgramada2.entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlunoRepository extends JpaRepository<Aluno, String> { }
